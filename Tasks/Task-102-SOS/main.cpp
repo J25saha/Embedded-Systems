9a8e6e48cedd04f1e6ddcf658b8208cdeb5f5e87
Joya  Saha <146337625+J25saha@users.noreply.github.com>
2023-10-05 12:58:40 +0100

    Completed
    Red, blue and green lights

--- Tasks/Task-102-SOS/main.cpp
+++ Tasks/Task-102-SOS/main.cpp
@@ -23,17 +23,46 @@ int main()
         //On for 500ms
         greenLED = 1;
         buzz.playTone("C");
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
+        wait_us(WAIT_TIME_MS * 500);  //500ms
+
+        //Off for 500ms
+        greenLED = 0;
+        buzz.playTone("D", Buzzer::HIGHER_OCTAVE);
+        wait_us(WAIT_TIME_MS * 500);  //500ms
+
+        greenLED = 1;
+        buzz.playTone("E");
+        wait_us(WAIT_TIME_MS * 500);  //500ms
+
+
+        //Off for 500ms
+        greenLED = 0;
+        buzz.playTone("F", Buzzer::HIGHER_OCTAVE);
+        wait_us(WAIT_TIME_MS * 500);  //500ms
+
+
+        greenLED = 1;
+        buzz.playTone("G");
+        wait_us(WAIT_TIME_MS * 500);  //500ms
+
+        //Off for 500ms
+        greenLED = 0;
+        buzz.playTone("A", Buzzer::HIGHER_OCTAVE);
+        wait_us(WAIT_TIME_MS * 500);  //500ms
+
+
+        greenLED = 1;
+        buzz.playTone("B");
+        wait_us(WAIT_TIME_MS * 500);  //500ms
 
 
         //Off for 500ms
         greenLED = 0;
         buzz.playTone("C", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
+        wait_us(WAIT_TIME_MS * 500);  //500ms
 
         //Pause
         buzz.rest();
         wait_us(WAIT_TIME_MS * 1000);
-
     }
 }
