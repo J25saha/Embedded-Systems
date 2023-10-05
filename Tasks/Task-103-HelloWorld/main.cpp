7e0a1d7343e9b6f21232f1c73fcedc4e35aaa3cd
Joya  Saha <146337625+J25saha@users.noreply.github.com>
2023-10-05 16:06:04 +0100

    session 2 (5/10/23)

--- Tasks/Task-103-HelloWorld/main.cpp
+++ Tasks/Task-103-HelloWorld/main.cpp
@@ -13,7 +13,7 @@ LCD_16X2_DISPLAY lcd;
 int main() 
 {
     // Write to serial terminal on host PC
-    printf("PUMPKIN!\n");
+    printf("Hello World\n");
 
     // Clear the LCD screen
     lcd.cls();
@@ -27,7 +27,7 @@ int main()
     ledBlue = 0;
 
     // Write to LCD using the LCD_16X2_DISPLAY driver class
-    lcd.printf("PUMPKIN!");
+    lcd.printf("Hello World");
     lcd.locate(1, 0);   //Row 1, Col 0
     lcd.printf("ELEC143");
 
--- Tasks/Task-104-TrafficLights/main.cpp
+++ Tasks/Task-104-TrafficLights/main.cpp
@@ -8,30 +8,35 @@ DigitalOut amber(TRAF_YEL1_PIN,0);
 DigitalOut green(TRAF_GRN1_PIN,0);
 
 LCD_16X2_DISPLAY lcd;
-
 int main()
 {
-    lcd.puts("RED");
-    wait_us(1000000);
-
-    amber = 1;
-    lcd.cls();
-    lcd.puts("Amber");
-    wait_us(1000000);
-
-    green = 1;
-    lcd.cls();
-    lcd.puts("Green");    
-    wait_us(1000000);
-
-    lcd.cls();
-    lcd.puts("TASK-104");
-
     while (true)
     {
-        red = !red;
-        amber = !amber;
-        green = !green;
-        wait_us(WAIT_TIME_MS * 1000);
+        lcd.puts("RED");
+        red = 1;
+        wait_us(10000000);
+
+        amber = 1;
+        lcd.cls();
+        lcd.puts("Amber");
+        wait_us(2000000);
+        
+        red = 0;
+        amber = 0;
+
+        green = 1;
+        lcd.cls();
+        lcd.puts("Green");    
+        wait_us(10000000);
+        
+        green = 0;
+
+        amber = 1;
+        lcd.cls();
+        lcd.puts("amber");
+        wait_us(2000000);
     }
+
 }
+
+
