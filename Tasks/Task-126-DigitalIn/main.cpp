e812a0e8117f717aa1ba29972a8e4254234a24c2
Joya  Saha <146337625+J25saha@users.noreply.github.com>
2023-11-09 15:54:40 +0000

    week 5

--- Tasks/Task-126-DigitalIn/main.cpp
+++ Tasks/Task-126-DigitalIn/main.cpp
@@ -2,11 +2,14 @@
 
 DigitalIn ButtonA(PG_0); //Button A
 DigitalOut redLED(PC_2); //Red Traffic 1
+DigitalIn ButtonB(PG_1);
+DigitalIn ButtonC(PG_2);
+DigitalIn ButtonD(PG_3);
 
 // main() runs in its own thread in the OS
 int main()
 {
-    int btnA;
+    int btnA, btnB, btnC, btnD;
     // Turn OFF the red LED
     redLED = 0;
 
@@ -15,7 +18,10 @@ int main()
         // Wait for the button to be pressed
         do {
             btnA = ButtonA; //Read button A
-        } while (btnA == 0);
+            btnB = ButtonB;
+            btnC = ButtonC;
+            btnD = ButtonD;
+        } while ((btnA == 0) || (btnB == 0) || (btnC == 0) || (btnD == 0));
 
 
         //Toggle the red LED
@@ -27,7 +33,10 @@ int main()
         // Wait for the button to be released
         do {
             btnA = ButtonA; //Read button A
-        } while (btnA == 1);
+            btnB = ButtonB;
+            btnC = ButtonC;
+            btnD = ButtonD;
+        } while ((btnA == 1) && (btnB == 1) && (btnC == 1) && (btnD == 1));
 
         //Wait for noise to settle
         wait_us(10000);
